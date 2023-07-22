7bc6cbab70dc6851284bee75cffe1a93fa6b3966
jihyunjoo2023 <smallpig10@naver.com>
2023-07-22 10:23:15 +0900

    "feature:Add header file"

--- src/layout/PageLayout.jsx
+++ src/layout/PageLayout.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
 import React from "react";
-import Header from "./Header";
+import Header from "./Header/Header";
 import Footer from "./Footer";
 
 export default function PageLayout() {
